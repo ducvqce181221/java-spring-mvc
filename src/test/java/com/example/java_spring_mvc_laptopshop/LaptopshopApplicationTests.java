f71013e154362afb23a2658721090a7648d04503
Quang Duc <vuquangduc1404@gmail.com>
2025-03-31 20:34:17 +0700

    first commit

---
+++ src/test/java/com/example/java_spring_mvc_laptopshop/LaptopshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_mvc_laptopshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
