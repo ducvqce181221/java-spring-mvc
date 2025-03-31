3f2f5798dab2c4da99f353fde5cce32474de7f36
Quang Duc <vuquangduc1404@gmail.com>
2025-03-31 22:57:41 +0700

    video35

--- src/main/java/com/example/java_spring_mvc_laptopshop/LaptopshopApplication.java
+++ src/main/java/com/example/java_spring_mvc_laptopshop/LaptopshopApplication.java
@@ -2,12 +2,17 @@ package com.example.java_spring_mvc_laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
+
+		ApplicationContext quangduck = SpringApplication.run(LaptopshopApplication.class, args);
+		for (String s : quangduck.getBeanDefinitionNames()) {
+			System.out.println(s);
+		}
 	}
 
 }
